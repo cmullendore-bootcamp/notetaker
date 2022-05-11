8a300a5dad41917a56978196af527b24c682bbf5
Chris Mullendore <chris@vindage.com>
2022-05-10 18:44:55 -0700

    Create Node class

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const Express = require('express');
 const FS = require('fs');
+const uuid = require("uuid");
+const Note = require('./lib/Note');
+
+/* class Note {
+    constructor(id, title, content, createdOn, updatedOn) {
+        this.Id = (id) ? id : uuid.v5.DNS;
+        this.Title = title;
+        this.Content = content;
+        this.CreatedOn = createdOn;
+        this.UpdatedOn = updatedOn;
+    }
+} */
 
 const app = Express();
+
+let note = new Note(null, "title1", "content1", "createdOn1", "updatedOn1");
 
 app.get('/', (req, res) => {
     let notes = FS.readFileSync('./public/index.html', "utf-8");
