77a44af7d7832ff455f0c39c28c72fed7fde4798
Chris Mullendore <chris@vindage.com>
2022-05-10 17:01:13 -0700

    Testing heroku

---
+++ server.js
@@ -0,0 +1,24 @@
+const Express = require('express');
+const FS = require('fs');
+
+const app = Express();
+
+app.get('/', (req, res) => {
+    let notes = FS.readFileSync('./public/index.html', "utf-8");
+    res.send(notes);
+});
+
+app.get('/notes', (req, res) => {
+    let notes = FS.readFileSync('./public/notes.html', "utf-8");
+    res.write(notes);
+});
+
+app.get('/api/notes', (req, res) => {
+    let db = FS.readFileSync('./db/db.json', "utf-8");
+    res.json(JSON.parse(db));
+});
+
+
+app.listen(443, () => {
+    console.log(`API server now on port 3001!`);
+});
